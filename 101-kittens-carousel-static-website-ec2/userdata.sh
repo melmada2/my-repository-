1a6067cd618637efb81ba15cf7ba7068c58fb57a
melmada2 <melmada2@gmail.com>
2024-03-30 13:25:32 -0400

    Updating files

--- 101-kittens-carousel-static-website-ec2/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 yum update -y
+#Install Apache
 yum install httpd -y
-wget $Folder=[]
+
+#Copy content to /var/www/html folder 
+cd /var/www/html
+Folder="https://raw.githubusercontent.com/melmada2/my-repository-/main/101-kittens-carousel-static-website-ec2/static-web"
+wget ${Folder}/index.html
+wget ${Folder}/cat0.jpg
+wget ${Folder}/cat1.jpg
+wget ${Folder}/cat2.jpg
+
+#Start and Enable Apache servie
+systemctl start httpd 
+systemctl enable httpd
