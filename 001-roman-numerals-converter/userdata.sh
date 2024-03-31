006fdfeb8a0aad17fe2812b9efe0cbdaf2e675e9
melmada2 <melmada2@gmail.com>
2024-03-31 15:30:08 -0400

    Updated files

--- 001-roman-numerals-converter/userdata.sh
+++ 001-roman-numerals-converter/userdata.sh
@@ -1,8 +1,18 @@
-#!/bin/bash
+#!/bin/bash -x 
 yum update -y
-yum install -y
 
-cd /var/www/html
-Folder=""
-wget ${Folder}/index.html
-wget ${Folder}/result.html
+#instal python and flask
+yum install python3 -y
+yum install pip -y
+pip3 install flask 
+
+#install application files
+cd /home/ec2-user
+mkdir templates
+FOLDER="https://raw.githubusercontent.com/melmada2/my-repository-/main/001-roman-numerals-converter"
+wget ${FOLDER}/app.py
+wget  -P templates ${FOLDER}/templates/index.html
+wget  -P templates ${FOLDER}/templates/result.html
+
+#run application 
+python3 app.py
